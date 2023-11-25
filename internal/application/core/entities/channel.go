f3d33da039fdc7950015c74399b50771426dd68e
Mohamed Sawy <mohamedsawy1022@gmail.com>
2023-11-25 18:34:10 +0200

    refactor(core): use field Members instead of Users

--- internal/application/core/entities/channel.go
+++ internal/application/core/entities/channel.go
@@ -17,12 +17,12 @@ type ServerChannel struct {
 	ServerID uuid.UUID `json:"server_id" gorm:"not null"`
 
 	Messages []ServerMessage `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Users    []ServerMember  `gorm:"many2many:server_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:UserID,ServerID;joinReferences:UserID,ServerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Members  []ServerMember  `gorm:"many2many:server_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:UserID,ServerID;joinReferences:UserID,ServerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type DMChannel struct {
 	Channel `gorm:"embedded"`
 
 	Messages []DirectMessage `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Users    []User          `gorm:"many2many:dm_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:ID;joinReferences:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Members  []User          `gorm:"many2many:dm_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:ID;joinReferences:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
