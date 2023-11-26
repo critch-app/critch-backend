d6b7a48a7e5e19107656eefd55089337bac708e3
Mohamed Sawy <mohamedsawy1022@gmail.com>
2023-11-26 11:45:59 +0200

    fix(core): fix models relationship
    make model relationships based on one-to-many only,
    to avoid depending on gorm join table setup,
    and allow adding more fields to the join tables.

--- internal/application/core/entities/channel.go
+++ internal/application/core/entities/channel.go
@@ -16,13 +16,13 @@ type ServerChannel struct {
 	Channel  `gorm:"embedded"`
 	ServerID uuid.UUID `json:"server_id" gorm:"not null"`
 
-	Messages []ServerMessage `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Members  []ServerMember  `gorm:"many2many:server_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:UserID,ServerID;joinReferences:UserID,ServerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Messages []ServerMessage       `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Members  []ServerChannelMember `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type DMChannel struct {
 	Channel `gorm:"embedded"`
 
-	Messages []DirectMessage `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Members  []User          `gorm:"many2many:dm_channel_members;foreignKey:ID;joinForeignKey:ChannelID;References:ID;joinReferences:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Messages []DirectMessage   `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Members  []DMChannelMember `gorm:"foreignKey:ChannelID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
--- internal/application/core/entities/user.go
+++ internal/application/core/entities/user.go
@@ -18,8 +18,10 @@ type User struct {
 	LastSeen  string    `json:"last_seen"`
 	CreatedAt time.Time `json:"created_at"`
 
-	DirectMessages []DirectMessage `gorm:"foreignKey:SenderID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	ServerMessages []ServerMessage `gorm:"foreignKey:SenderID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	DirectMessages []DirectMessage   `gorm:"foreignKey:SenderID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	ServerMessages []ServerMessage   `gorm:"foreignKey:SenderID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Servers        []ServerMember    `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	DMChannels     []DMChannelMember `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type ServerMember struct {
@@ -27,6 +29,8 @@ type ServerMember struct {
 	UserID   uuid.UUID `json:"user_id" gorm:"primaryKey"`
 	Role     string    `json:"role" gorm:"check:role IN ('owner', 'admin', 'member');default:member"`
 	JoinedAt time.Time `json:"joined_at" gorm:"autoCreateTime"`
+
+	Channels []ServerChannelMember `gorm:"foreignKey:UserID, ServerID;references:UserID, ServerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type DMChannelMember struct {
